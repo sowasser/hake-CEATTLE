b50ed1064b140a29937f77bace6039904ff3d9d2
Sophia Wassermann <sowasser@uw.edu>
2021-11-05 11:55:07 -0700

    Added more length/weight at age plots & added .pdf to gitignore

--- hake_graphs.R
+++ hake_graphs.R
@@ -4,8 +4,33 @@
 library(ggplot2)
 library(ggsidekick)
 
-maturity <- read.csv("~/Desktop/Local/hake-assessment-master/data/maturity-table.csv")
 
-ggplot(maturity, aes(x = age, y = avg.wt)) +
+# Weight & length at age ------------------------------------------------------
+maturity_table <- read.csv("~/Desktop/Local/hake-assessment-master/data/maturity-table.csv")
+hake_maturity_data <- read.csv("~/Desktop/Local/hake-assessment-master/data/hake-maturity-data.csv")
+
+weight_age <- ggplot(maturity_table, aes(x=age, y=avg.wt)) +
   geom_point() +
   theme_sleek()
+# weight_age
+
+ggsave(filename="plots/weight_age.pdf", weight_age,
+       width=150, height=100, units="mm", dpi=300)
+
+weight_age_yearly <- ggplot(hake_maturity_data, aes(x=Age, y=Weight_kg)) +
+  geom_point() +
+  theme_sleek() +
+  facet_wrap(~Year, ncol = 3) 
+# weight_age_yearly
+
+ggsave(filename="plots/weight_age_yearly.pdf", weight_age_yearly,
+       width=300, height=200, units="mm", dpi=300)
+
+length_age_yearly <- ggplot(hake_maturity_data, aes(x=Age, y=Length_cm)) +
+  geom_point() +
+  theme_sleek() +
+  facet_wrap(~Year, ncol = 3) 
+# length_age_yearly
+
+ggsave(filename="plots/length_age_yearly.pdf", length_age_yearly,
+       width=300, height=200, units="mm", dpi=300)
