d824446f7ec1308a8e295013d48824f2940df8bb
Sophia Wassermann <sowasser@uw.edu>
2021-11-10 18:37:43 -0800

    Added all sources of catch data

--- hake_graphs.R
+++ hake_graphs.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(ggsidekick)
 library(reshape2)
 library(viridis)
+library(dplyr)
 
 path <- "~/Desktop/Local/hake-assessment-master/data/"
 
@@ -97,21 +98,56 @@ ggsave(filename="plots/catch_age_comp.pdf", catch_age_comp,
 
 
 # Catch rates -----------------------------------------------------------------
-can_ft_catch_by_month <- read.csv(paste0(path, "can-ft-catch-by-month.csv"))
-can_jv_catch_by_month <- read.csv(paste0(path, "can-jv-catch-by-month.csv"))
-can_ss_catch_by_month <- read.csv(paste0(path, "can-ss-catch-by-month.csv"))
+# Import data, rename columns, add column with country & source names, reshape
+catch_data <- function(file, country, type) {
+  df <- read.csv(paste0(path, file))
+  
+  if (country == "Canada") {
+    colnames(df) <- c("year", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    df2 <- cbind(df, nation = rep("Canada", length(df[, 1])), 
+                 source = rep(type, length(df[, 1])))
+    df3 <- melt(df2, id.vars = c("year", "nation", "source"), 
+                variable.name = "month", value.name = "catch")
+    return(df3)
+  }
+  
+  if (country == "US") {
+    df2 <- cbind(year = df[, 2], nation = rep("US", length(df[, 2])), 
+                 source = rep(type, length(df[, 2])), month = df[, 1], 
+                 catch = df[, 3])
+    return(as.data.frame(df2))
+  }
 
-us_catch_by_month <- read.csv(paste0(path, "us-catch-by-month.csv"))
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+}
 
-monthly_catch <- ggplot(us_catch_by_month, aes(y=Catch.MT, x=month)) +
-  geom_bar(position = "dodge", stat = "identity") +
+
+us_unid <- read.csv(paste0(path, "us-catch-by-month.csv"))
+us_unid <- as.data.frame(cbind(year = us_unid[, 3], nation = rep("US", length(us_unid[, 3])), 
+                               source = rep("unidentified", length(us_unid[, 3])), month = us_unid[, 2], 
+                               catch = us_unid[, 4]))
+
+monthly_catch <- rbind(catch_data("can-ft-catch-by-month.csv", "Canada", "freezer-trawler"),
+                       catch_data("can-jv-catch-by-month.csv", "Canada", "JV"),
+                       catch_data("can-ss-catch-by-month.csv", "Canada", "shoreside"),
+                       catch_data("us-cp-catch-by-month.csv", "US", "catcher-processor"),
+                       catch_data("us-ms-catch-by-month.csv", "US", "mothership"),
+                       catch_data("us-research-catch-by-month.csv", "US", "research"),
+                       catch_data("us-shore-catch-by-month.csv", "US", "shore-based"),
+                       us_unid)
+monthly_catch$catch <- as.numeric(monthly_catch$catch)
+all_catch <- monthly_catch %>% group_by(year, nation, source) %>%
+  summarize(total_catch = sum(catch))
+
+catch <- ggplot(all_catch, aes(y=total_catch, x=year, fill=source)) +
+  geom_bar(stat = "identity") +
+  scale_fill_viridis(discrete = TRUE) +
   theme_sleek() +
-  facet_wrap(~year, ncol=3)
-# monthly_catch
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  facet_wrap(~nation, ncol=2)
 
-ggsave(filename="plots/monthly_catch.pdf", monthly_catch,
-       width=300, height=300, units="mm", dpi=300)
+ggsave(filename="plots/catch.pdf", catch,
+       width=400, height=200, units="mm", dpi=300)
+
 
 # Survey ----------------------------------------------------------------------
 survey_history <- read.csv(paste0(path, "survey-history.csv"))
