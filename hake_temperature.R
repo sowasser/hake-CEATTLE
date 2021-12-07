ed199339f13e2d44781c69ee775bfa73630d7b97
Sophia Wassermann <hi@sowasser.com>
2021-12-07 12:53:34 -0800

    Added max temp & temp histograms for parameterizing the bioenergetics

--- hake_temperature.R
+++ hake_temperature.R
@@ -33,12 +33,46 @@ all_years$temp <- as.numeric(all_years$temp)
 all_years$year <- as.integer(all_years$year)
 
 # Plot all mean temperatures 
-temp_plot <- ggplot(all_years, aes(x=year, y=temp)) +
+mean_temp_plot <- ggplot(all_years, aes(x=year, y=temp)) +
   geom_line(color="gray") +
   geom_point(aes(color=data_type)) +
   scale_color_viridis(discrete = TRUE, direction=-1) +  # invert colors
   theme_sleek()
-# temp_plot
+# mean_temp_plot
 
-ggsave(filename="plots/survey_mean_temp.pdf", temp_plot,
+ggsave(filename="plots/survey_mean_temp.pdf", mean_temp_plot,
+       width=150, height=100, units="mm", dpi=300)
+
+# Temp distribution & max for looking at hake thermal maximum -----------------
+# overall maximum
+overall_max <- max(temp_all$temp_100)  # 14.481°C
+
+# Max temperature for each year
+year_max <- temp_all %>% group_by(year) %>%
+  filter(temp_100 == max(temp_100))
+
+max_temp <- ggplot(year_max, aes(x=year, y=temp_100)) +
+  geom_point() +
+  theme_sleek()
+max_temp
+
+ggsave(filename="plots/survey_max_temp.pdf", max_temp,
+       width=150, height=100, units="mm", dpi=300)
+
+# Temperature distribution overall and per year
+temp_dist_all <- ggplot(temp_all, aes(x=temp_100)) +
+  geom_histogram() +
+  theme_sleek() 
+temp_dist_all
+
+ggsave(filename="plots/temp_dist_all.pdf", temp_dist_all,
+       width=150, height=100, units="mm", dpi=300)
+
+temp_dist_years <- ggplot(temp_all, aes(x=temp_100)) +
+  geom_histogram() +
+  theme_sleek() +
+  facet_wrap(~year)
+# temp_dist_years
+
+ggsave(filename="plots/temp_dist_years.pdf", temp_dist_years,
        width=150, height=100, units="mm", dpi=300)
