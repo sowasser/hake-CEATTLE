056f5e39b7299c4ed3915b474bb4fb9a85ace84d
Sophia Wassermann <hi@sowasser.com>
2021-12-10 13:55:17 -0800

    Ran equations for the FB4 values that use equation 2

--- hake_bioenergetics.R
+++ hake_bioenergetics.R
@@ -2,10 +2,20 @@
 # parameters available for similar species - Walleye pollock and Atlantic cod.
 # Data are from the Fish Bioenergetics 4.0 shiny app: http://fishbioenergetics.org/
 
+library(reshape2)
+library(ggplot2)
+library(ggsidekick)
+library(viridis)
+
 proxy_params <- read.csv("data/bioenergetics/proxy_bioen_params.csv")
 
+eq_1 <- proxy_params[c(2:4), ]
+eq_2 <- proxy_params[c(1, 5:6), ]
+eq_2$Tco <- as.numeric(eq_2$Tco)
+eq_2$Tcm <- as.numeric(eq_2$Tcm)
+
 # Temperature-dependent consumption -------------------------------------------
-temp_range <- 5:15  # Hake thermal range from survey data
+temp_range <- seq(1, 20, by = 0.1)  # Hake thermal range from survey data
 
 temp_dependent <- function(Qc, Tco, Tcm) {
   z <- (log(Qc) * (Tcm - Tco)) 
@@ -23,10 +33,26 @@ temp_dependent <- function(Qc, Tco, Tcm) {
   return(consumption)
 }
 
-test <- temp_dependent(2, 10, 15)
+# Run equation with cod, adult, juvenile pollock parameters
+spp_temp_wide <- cbind(temp_dependent(eq_2[1, 5], eq_2[1, 6], eq_2[1, 7]), 
+                       temp_dependent(eq_2[2, 5], eq_2[2, 6], eq_2[2, 7]), 
+                       temp_dependent(eq_2[3, 5], eq_2[3, 6], eq_2[3, 7]))
+colnames(spp_temp_wide) <- c("Atlantic cod", "pollock (adult)", "pollock (juvenile)")
+spp_temp <- melt(as.data.frame(spp_temp_wide))
+spp_temp <- cbind(spp_temp, temp = rep(temp_range, times=3))
+
+temp_rate <- ggplot(spp_temp, aes(x=temp, y=value)) +
+  geom_line(aes(color=variable), size=1) +
+  scale_color_viridis(discrete = TRUE) +  # invert colors
+  theme_sleek() +
+  ylab("specific rate") +
+  labs(color = "species")
+
+ggsave(filename="plots/bioenergetics/temp_consumption.pdf", temp_rate,
+       width=200, height=100, units="mm", dpi=300)
 
 # Allometric mass function ----------------------------------------------------
-weights <- 100:400  # Range of hake weights
+weights <- 10:400  # Range of hake weights
   
 allometric_mass <- function(CA, CB) {
   Cmax <- c()
@@ -37,4 +63,20 @@ allometric_mass <- function(CA, CB) {
   return(Cmax)
 }
 
-test2 <- allometric_mass(0.3, -0.3)
+# Run equation with cod, adult, juvenile pollock parameters
+spp_mass_wide <- cbind(allometric_mass(eq_2[1, 3], eq_2[1, 4]), 
+                       allometric_mass(eq_2[2, 3], eq_2[2, 4]), 
+                       allometric_mass(eq_2[3, 3], eq_2[3, 4]))
+colnames(spp_mass_wide) <- c("Atlantic cod", "pollock (adult)", "pollock (juvenile)")
+spp_mass <- melt(as.data.frame(spp_mass_wide))
+spp_mass <- cbind(spp_mass, weight = rep(weights, times=3))
+
+mass_rate <- ggplot(spp_mass, aes(x=weight, y=value)) +
+  geom_line(aes(color=variable), size=1) +
+  scale_color_viridis(discrete = TRUE) +  # invert colors
+  theme_sleek() +
+  ylab("specific rate") +
+  labs(color = "species")
+
+ggsave(filename="plots/bioenergetics/allometric_mass.pdf", mass_rate,
+       width=200, height=100, units="mm", dpi=300)
