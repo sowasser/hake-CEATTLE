d9e2a09f7c133bb0fd0a0d36e3a1b703f16b514b
Sophia Wassermann <hi@sowasser.com>
2022-01-19 16:53:54 -0800

    Added length & weight histograms for specimens & fixed bin #s

--- hake_diet.R
+++ hake_diet.R
@@ -7,23 +7,49 @@ library(ggsidekick)
 
 specimens <- read.csv("data/diet/hake_specimens.csv")
 contents <- read.csv("data/diet/hake_contents.csv")
+combined <- read.csv("data/diet/diet_combined.csv")
 
-
-# Stomach contents only -------------------------------------------------------
-# Prey lengths 
-lengths <- ggplot(prey, aes(x=length)) +
+# Length & weight -------------------------------------------------------------
+# Contents lengths 
+contents_lengths <- ggplot(contents, aes(x=length)) +
   geom_histogram() +
+  stat_bin(bins=4) +
   theme_sleek() +
   xlab("content length (cm)")
+contents_lengths
 
-ggsave(filename="plots/diet/contents_length.png", lengths,
+ggsave(filename="plots/diet/contents_length.png", contents_lengths,
        width=150, height=100, units="mm", dpi=300)
 
-# Prey weights
-weights <- ggplot(prey, aes(x=content_weight)) +
+# Contents weights
+contents_weights <- ggplot(contents, aes(x=content_weight)) +
   geom_histogram() +
+  stat_bin(bins=5) +
   theme_sleek() +
   xlab("weight (g)")
+contents_weights
 
-ggsave(filename="plots/diet/contents_length.png", lengths,
+ggsave(filename="plots/diet/contents_weight.png", contents_weight,
+       width=150, height=100, units="mm", dpi=300)
+
+# Specimen lengths
+specimen_lengths <- ggplot(combined, aes(x=fork_length)) +
+  geom_histogram() +
+  stat_bin(bins=5) +
+  theme_sleek() +
+  xlab("specimen length (cm)")
+specimen_lengths
+
+ggsave(filename="plots/diet/specimen_lengths.png", specimen_lengths,
+       width=150, height=100, units="mm", dpi=300)
+
+# Specimen weights
+specimen_weights <- ggplot(combined, aes(x=organism_weight_kg)) +
+  geom_histogram() +
+  stat_bin(bins=5) +
+  theme_sleek() +
+  xlab("organism weight (kg)")
+specimen_weights
+
+ggsave(filename="plots/diet/specimen_weights.png", specimen_weights,
        width=150, height=100, units="mm", dpi=300)
