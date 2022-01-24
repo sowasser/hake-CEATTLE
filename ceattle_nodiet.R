9a3a62cc0218cc13fc8ccdcd1bb207e1341aeff2
Sophia Wassermann <hi@sowasser.com>
2022-01-23 18:21:21 -0800

    So actually there was a function for reading in data

--- ceattle_nodiet.R
+++ ceattle_nodiet.R
@@ -1,15 +1,6 @@
 # Run CEATTLE for hake data with no diet data 
 
 library(Rceattle)
-library(readxl)
 
-# Read in data from excel file ------------------------------------------------
-read_excel_data <- function(filename, tibble = FALSE) {
-  sheets <- readxl::excel_sheets(filename)
-  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
-  if(!tibble) x <- lapply(x, as.data.frame)
-  names(x) <- sheets
-  x
-}
-
-hake_nodiet <- read_excel_data("data/input_nodiet.xlsx")
+mydata <- Rceattle::read_data( file = "data/input_nodiet.xlsx")
+mydata$est_M1 <- c(0,0,0)
