5d0d87b8588553ee7b0a9503e3b1cc2f3aa60bfa
Sophia Wassermann <hi@sowasser.com>
2022-02-07 17:17:57 -0800

    Added some plots of all of the predators in the stomach dataset

--- hake_diet.R
+++ hake_diet.R
@@ -9,6 +9,7 @@ library(fishmethods)
 
 stomach_summary <- read.csv("data/diet/hake_stomachs_summary.csv")
 full_stomachs <- read.csv("data/diet/full_stomachs.csv")
+hake_predator <- read.csv("data/diet/hake_predator_all.csv")
 
 # Total stomachs per year -----------------------------------------------------
 per_year <- ggplot(stomach_summary, aes(x=year)) +
@@ -18,8 +19,45 @@ per_year <- ggplot(stomach_summary, aes(x=year)) +
   xlab("year") + ylab(" ")
 per_year
 
+ggsave(filename="plots/diet/stomachs_per_year.png", per_year,
+       width=150, height=100, units="mm", dpi=300)
 
-# Length & weight -------------------------------------------------------------
+# Predator hake dynamics ------------------------------------------------------
+# Overall lengths 
+pred_lengths <- ggplot(hake_predator, aes(x=fork_length.cm, fill=sex)) +
+  geom_histogram() +
+  # stat_bin(binwidth = 3) +
+  theme_sleek() +
+  scale_fill_viridis(discrete = TRUE) +
+  xlab("fork length (cm)") + ylab(" ")
+pred_lengths
+
+ggsave(filename="plots/diet/all_pred_lengths.png", pred_lengths,
+       width=150, height=100, units="mm", dpi=300)
+
+pred_age <- ggplot(hake_predator, aes(x=age, fill=sex)) +
+  geom_histogram() +
+  # stat_bin(binwidth = 3) +
+  theme_sleek() +
+  scale_fill_viridis(discrete = TRUE) +
+  xlab("age") + ylab(" ")
+pred_age
+
+ggsave(filename="plots/diet/pred_age.png", pred_age,
+       width=150, height=100, units="mm", dpi=300)
+
+pred_sex <- ggplot(hake_predator, aes(x=year, fill=sex)) +
+  geom_bar(stat="count") +
+  theme_sleek() +
+  scale_fill_viridis(discrete = TRUE) +
+  xlab("year") + ylab(" ")
+pred_sex
+
+ggsave(filename="plots/diet/pred_sex.png", pred_sex,
+       width=150, height=100, units="mm", dpi=300)
+
+
+# Length & weight of predator/prey --------------------------------------------
 lengths <- as.data.frame(
   rbind(cbind(na.omit(full_stomachs$content_length.cm), 
               rep("prey", times = length(na.omit(full_stomachs$content_length.cm)))),
@@ -34,7 +72,7 @@ length_hist <- ggplot(lengths, aes(x=length, fill=hake)) +
   geom_histogram() +
   stat_bin(binwidth = 3) +
   theme_sleek() +
-  scale_fill_viridis(discrete = TRUE, begin = 0.25, end = 0.75) +
+  scale_fill_viridis(discrete = TRUE, begin = 0.25, end = 0.7) +
   xlab("length (cm)") + ylab(" ")
 length_hist
 
