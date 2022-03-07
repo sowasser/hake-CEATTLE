3c2aac3367849be041835e38e419ca7b6fa9157c
Sophia Wassermann <hi@sowasser.com>
2022-03-07 13:33:37 -0800

    Updated to a hindcast starting in 1980
    This matches the reliable ROMS temperature data.

--- ceattle_nodiet.R
+++ ceattle_nodiet.R
@@ -1,6 +1,6 @@
 # Run CEATTLE for hake data with no diet data 
-# devtools::install_github("grantdadams/Rceattle@dev")
-# library(Rceattle)
+devtools::install_github("grantdadams/Rceattle@dev")
+library(Rceattle)
 
 library(r4ss)
 library(reshape2)
@@ -11,10 +11,7 @@ library(viridis)
 
 # Run CEATTLE -----------------------------------------------------------------
 # mydata <- Rceattle::read_data( file = "data/hake_from_ss.xlsx")
-hake_nodiet <- Rceattle::read_data( file = "data/hake220303.xlsx")
-
-# mydata$projyr <- 2022  # change to 2022 to match assessment
-hake_nodiet$projyr <- 2022  # change to 2022 to match assessment
+hake_nodiet <- Rceattle::read_data( file = "data/hake220307.xlsx")
 
 # ss_run <- Rceattle::fit_mod(data_list = mydata,
 #                             inits = NULL, # Initial parameters = 0
@@ -35,61 +32,58 @@ nodiet_run <- Rceattle::fit_mod(data_list = hake_nodiet,
 # Check what all comes out of CEATTLE
 ceattle_stuff <- nodiet_run$quantities
 
+years <- 1980:2022
+
 
 # Compare spawning stock biomass & total biomass between SS3 and CEATTLE ------
 # Pull out SSB & total biomass from CEATTLE & combine
 ceattle_ssb <- (c(nodiet_run$quantities$biomassSSB) * 2)
 ceattle_biomass <- c(nodiet_run$quantities$biomass)
 
-ceattle_biom_wide <- as.data.frame(cbind(1966:2022, ceattle_ssb, ceattle_biomass))
+ceattle_biom_wide <- as.data.frame(cbind(years, ceattle_ssb, ceattle_biomass))
 colnames(ceattle_biom_wide) <- c("year", "SSB", "Total Biomass")
 ceattle_biom <- melt(ceattle_biom_wide, id.vars = "year")
 colnames(ceattle_biom)[2:3] <- c("type", "CEATTLE")
 
-# Pull out SSB & total biomass from stock synthesis & combine
+# Pull out SSB & total biomass from stock synthesis & combine, remove pre-1980
 ss_ssb_werror <- read.table("data/assessment/ssb.txt")
-ss_ssb <- ss_ssb_werror[, 2]
+ss_ssb <- ss_ssb_werror[15:57, 2]
 
 ss_biomass <- read.table("data/assessment/biomass.txt")
-ss_biom <- ss_biomass[, 2]
+ss_biom <- ss_biomass[15:57, 2]
 
-ss_biom_wide <- as.data.frame(cbind(1966:2022, ss_ssb, ss_biom))
+ss_biom_wide <- as.data.frame(cbind(years, ss_ssb, ss_biom))
 colnames(ss_biom_wide) <- c("year", "SSB", "total biomass")
 ss_biom_all <- melt(ss_biom_wide, id.vars = "year")
-
-# Error from stock synthesis SSB
-error <- c(rep(0, 114), ss_ssb_werror[, 3], rep(0, 57))
 
 # Combine all together and plot
 biom_wide <- cbind(ceattle_biom, ss_biom_all[, 3])
 colnames(biom_wide)[4] <- "Stock Synthesis"
-biom_noerror <- melt(biom_wide, id.vars = c("year", "type"))
-biom <- cbind(biom_noerror, error)
+biom <- melt(biom_wide, id.vars = c("year", "type"))
 
 biom_plot <- ggplot(biom, aes(x=year, y=value)) +
-  geom_line(aes(color=variable, linetype=type)) +
-  scale_linetype_manual(values=c("dashed", "solid")) +  # specify line types
-  # geom_errorbar(aes(ymin=value-error, ymax=value+error, color=variable), width=0, alpha=0.3) +
+  geom_line(aes(color=variable)) +
   scale_color_viridis(discrete = TRUE, begin = 0.1, end = 0.9) +  # specify colors
   theme_sleek() +
   ylab("Biomass (mt)") +
-  labs(color = "model")
+  labs(color = "model") +
+  facet_wrap(~type, ncol = 1)
 biom_plot
 
 ggsave(filename="plots/CEATTLE/allbiom_ss3_ceattle.png", biom_plot,
-       width=200, height=100, units="mm", dpi=300)
+       width=200, height=150, units="mm", dpi=300)
 
 
 # Compare recruitment between SS3 and CEATTLE ---------------------------------
 ceattle_R <- c(nodiet_run$quantities$R)
-ss_R <- read.table("data/assessment/recruitment.txt")
+ss_R <- read.table("data/assessment/recruitment.txt")[15:57,]
 
-recruitment_wide <- as.data.frame(cbind(1966:2022, ceattle_R, ss_R[, 2]))
+recruitment_wide <- as.data.frame(cbind(years, ceattle_R, ss_R[, 2]))
 colnames(recruitment_wide) <- c("year", "CEATTLE", "Stock Synthesis")
 recruitment <- melt(recruitment_wide, id.vars = "year")
 
 # Offset the stock synthesis data by one year (min age in CEATTLE is 1; in SS is 0)
-ss_1 <- cbind(1967:2022, rep("SS + 1", 56), ss_R[1:56, 2])
+ss_1 <- cbind(1981:2022, rep("SS + 1", (length(years)-1)), ss_R[1:42, 2])
 colnames(ss_1) <- c("year", "variable", "value")
 recruitment <- rbind(recruitment, ss_1)
 recruitment$value <- as.numeric(recruitment$value)
