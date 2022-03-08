f7769e574ae62ec5f721478cdb6bf9e4e879b442
Sophia Wassermann <hi@sowasser.com>
2022-03-07 16:25:04 -0800

    Starting to add another species to the CEATTLE data

--- ceattle_nodiet.R
+++ ceattle_nodiet.R
@@ -13,14 +13,6 @@ library(viridis)
 # mydata <- Rceattle::read_data( file = "data/hake_from_ss.xlsx")
 hake_nodiet <- Rceattle::read_data( file = "data/hake_singlesp_220307.xlsx")
 
-# ss_run <- Rceattle::fit_mod(data_list = mydata,
-#                             inits = NULL, # Initial parameters = 0
-#                             file = NULL, # Don't save
-#                             # debug = 1, # 1 = estimate, 0 = don't estimate
-#                             random_rec = FALSE, # No random recruitment
-#                             msmMode = 0, # Single species mode
-#                             phase = "default")
-
 nodiet_run <- Rceattle::fit_mod(data_list = hake_nodiet,
                               inits = NULL, # Initial parameters = 0
                               file = NULL, # Don't save
