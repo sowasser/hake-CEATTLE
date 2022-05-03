fafacf340ac9fd59741c1cb046c5b091682293d4
Sophia Wassermann <hi@sowasser.com>
2022-05-03 16:32:20 -0700

    Removed nbyage plot from no diet & pulled estim survey biomass from run

--- ceattle_nodiet.R
+++ ceattle_nodiet.R
@@ -55,57 +55,13 @@ colnames(nbyage) <- c("age", "year", "numbers")
 
 write.csv(nbyage, "data/ceattle_nodiet_nbyage.csv", row.names = FALSE)
 
-# Read in and transform data from stock synthesis - with & without age0 and for
-# beginning and middle of the year to find best match for CEATTLE
-nbyage_ss_all <- read.csv("data/assessment/nbyage.csv")
 
-b_wide <- nbyage_ss_all[1:43, -c(2:3)]
-m_wide <- nbyage_ss_all[44:86, -c(2:3)]
+# Extract survey biomass estimate and save ------------------------------------
+nodiet_srv <- data.frame(year = 1995:2019,
+                         biomass = nodiet_run$quantities$srv_bio_hat,
+                         log_sd = nodiet_run$quantities$srv_log_sd_hat)
 
-new_nbyage <- function(df, name) {
-  colnames(df) <- c("year", as.character(1:20))
-  df2 <- melt(df, id.vars = "year")
-  df3 <- cbind(df2, rep(name, nrow(df2)))
-  colnames(df3)[2:4] <- c("age", "numbers", "source")
-  
-  return(df3)
-}
-
-b <- new_nbyage(b_wide, "SS - beginning")
-m <- new_nbyage(m_wide, "SS - middle")
-
-nbyage_ss <- rbind(b, m)
-
-# Combine with CEATTLE & plot both per-year and mean
-nbyage_ss <- nbyage_ss[, c(2, 1, 3, 4)]
-nbyage_ceattle <- cbind(nbyage, rep("CEATTLE", nrow(nbyage)))
-colnames(nbyage_ceattle)[4] <- "source"
-
-nbyage_final <- rbind(nbyage_ceattle, nbyage_ss)
-
-nbyage_plot_year <- ggplot(nbyage_final, aes(x=age, y=numbers, fill=source)) +
-  geom_bar(stat = "identity", position = "dodge", ) +
-  theme_sleek() +
-  scale_fill_viridis(discrete = TRUE) +
-  xlab("age") + ylab("numbers") +
-  facet_wrap(~year)
-nbyage_plot_year
-
-ggsave(filename = "plots/CEATTLE/nbyage_year.png", nbyage_plot_year, 
-       width=500, height=400, units="mm", dpi=300)
-
-nbyage_mean <- nbyage_final %>% group_by(age, source) %>%
-  summarise(mean_number = mean(numbers))
-
-nbyage_plot_mean <- ggplot(nbyage_mean, aes(x=age, y=mean_number, fill=source)) +
-  geom_bar(stat = "identity", position = "dodge", ) +
-  theme_sleek() +
-  scale_fill_viridis(discrete = TRUE) +
-  xlab("age") + ylab("numbers") 
-nbyage_plot_mean
-
-ggsave(filename = "plots/CEATTLE/nbyage_mean.png", nbyage_plot_mean, 
-       width=200, height=120, units="mm", dpi=300)
+write.csv(nodiet_srv, "data/ceattle_nodiet_survey.csv", row.names = FALSE)
 
 
 # # Run r4ss diagnostics on stock synthesis model -------------------------------
