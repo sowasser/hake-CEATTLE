8a18fb96fec467438f920d6b0b6be17e1f1d5185
Sophia Wassermann <hi@sowasser.com>
2022-05-03 16:33:48 -0700

    Plotted estimated survey biomass from CEATTLE v actual # from assessment

--- ceattle_intrasp.R
+++ ceattle_intrasp.R
@@ -11,8 +11,11 @@ intrasp_run <- Rceattle::fit_mod(data_list = hake_intrasp,
                                  msmMode = 0, # Single species mode
                                  phase = "default")
 
+# Check what all comes out of CEATTLE
+ceattle_stuff <- intrasp_run$quantities
 
-# Plot biomass in comparison to no diet & asssessment -------------------------
+
+### Plot biomass in comparison to no diet & asssessment -----------------------
 years <- 1980:2022
 
 # Pull out SSB & overall biomass from CEATTLE runs
@@ -68,7 +71,7 @@ ggsave(filename="plots/CEATTLE/intraspecies predation/intrasp_biomass.png",
        biom_plot, width=200, height=150, units="mm", dpi=300)
 
 
-# Plot recruitment ------------------------------------------------------------
+### Plot recruitment ----------------------------------------------------------
 nodiet_R <- read.csv("data/ceattle_nodiet_R.csv")
 ss_R <- read.table("data/assessment/recruitment.txt")[15:57,]
 
@@ -103,7 +106,7 @@ ggsave(filename="plots/CEATTLE/intraspecies predation/intrasp_R.png",
        R_plot, width=200, height=100, units="mm", dpi=300)
 
 
-# Numbers-at-age for each model run -------------------------------------------
+### Numbers-at-age for each model run -----------------------------------------
 # Read in data from no diet CEATTLE run
 nbyage_nodiet <- read.csv("data/ceattle_nodiet_nbyage.csv")
 nbyage_nodiet <- cbind(nbyage_nodiet, rep("CEATTLE - no diet", nrow(nbyage_nodiet)))
@@ -153,3 +156,28 @@ nbyage_plot_mean
 
 ggsave(filename = "plots/CEATTLE/intraspecies predation/nbyage_intrasp.png", 
        nbyage_plot_mean, width=200, height=120, units="mm", dpi=300)
+
+
+### Compare survey biomass estimate from CEATTLE to true values ---------------
+nodiet_srv <- read.csv("data/ceattle_nodiet_survey.csv")
+nodiet_srv <- cbind(nodiet_srv, model = rep("CEATTLE - no diet", length(nodiet_srv$year)))
+
+survey <- read.csv("data/assessment/survey_data.csv")
+survey <- cbind(survey, model = rep("assessment", length(survey$year)))
+
+intrasp_srv <- data.frame(year = 1995:2019,
+                          biomass = intrasp_run$quantities$srv_bio_hat,
+                          log_sd = intrasp_run$quantities$srv_log_sd_hat,
+                          model = rep("CEATTLE - cannibalism", length(1995:2019)))
+
+survey_all <- rbind(intrasp_srv, nodiet_srv, survey)
+
+survey_plot <- ggplot(survey_all, aes(x=year, y=biomass, color=model)) +
+  geom_line() +
+  geom_ribbon(aes(ymin=(biomass-log_sd), ymax=(biomass+log_sd), fill=model)) +
+  theme_sleek() +
+  scale_color_viridis(discrete = TRUE, direction = -1, begin = 0.1, end = 0.9) +
+  scale_fill_viridis(discrete = TRUE, direction = -1, begin = 0.1, end = 0.9) +
+  xlab("year") + ylab("survey biomass") 
+survey_plot
+
