acf85f2925059a4701cd5186e7be9d8d5fe9c58c
Sophia Wassermann <hi@sowasser.com>
2022-06-01 14:57:49 +0000

    Towards multisp -  Created function for initial diet data combination

--- data/diet/all_diet_data.R
+++ data/diet/all_diet_data.R
@@ -12,112 +12,106 @@ library(purrr)
 
 path <- "data/diet/Full dataset/v4/"
 
-### Read in and subset datasets -----------------------------------------------
-# All collection info
-collection <- read.csv(paste0(path, "collection_information_v4.csv"))
-
-# Filter predator dataset for just hake and collections from 1980 onwards
-predator_hake <- read.csv(paste0(path, "predator_information_v4.csv")) %>%
-  filter(Predator_Com_Name == "Pacific Hake") %>%
-  filter(Collection_ID > 58) %>%  # Years before 1980
-  filter(!is.na(FL_cm))  # Remove observations without fork lengths
-
-# Filter collection info by hake predator collection IDs
-collection_hake <- read.csv(paste0(path, "collection_information_v4.csv")) %>%
-  filter(Collection_ID %in% predator_hake$Collection_ID)
-
-# Filter prey composition dataset by hake predator IDs
-prey_of_hake_comp <- read.csv(paste0(path, "prey_composition_v4.csv")) %>%
-  filter(Predator_ID %in% predator_hake$Predator_ID)
-
-# Filter prey size dataset by hake-predated-prey IDs
-prey_of_hake_size <-  read.csv(paste0(path, "prey_size_v4.csv")) %>%
-  filter(Prey_Comp_ID %in% prey_of_hake_comp$Prey_Comp_ID)
-
-
-### Combine then refine datasets ----------------------------------------------
-# Combine collection info with predator info
-all_pred <- merge(collection_hake, predator_hake, all.y = TRUE)[, c("Month", 
-                                                                    "Year", 
-                                                                    "Latitude", 
-                                                                    "Longitude",
-                                                                    "Predator_ID", 
-                                                                    "FL_cm", 
-                                                                    "Predator_Weight_kg")]
-
-# Combine prey-of-hake datasets together
-all_prey <- merge(prey_of_hake_comp, prey_of_hake_size, all.y = TRUE)[, c("Prey_Com_Name", 
-                                                                          "Predator_ID", 
-                                                                          "Prey_Weight_g",
-                                                                          "Prey_Maturity",
-                                                                          "Prey_Length1")]
-
-
-# Combine pred & prey togehter - this is just full hake stomachs.
-full_stomachs <- merge(all_pred, all_prey, all.y = TRUE)
-
-# Cannibalism instances
-hake_hake <- full_stomachs %>%
-  filter(Prey_Com_Name == "Pacific Hake")
-
-# Label predator dataframe by instances of cannibalism & change order for plots
-pred_type <- all_pred
-pred_type$type <- ifelse(pred_type$Predator_ID %in% hake_hake$Predator_ID, "cannibalistic", "general predator")
-pred_type$type <- relevel(factor(pred_type$type), "general predator")
-
-
-### Plot general trends in data -----------------------------------------------
-# Overall number of collections
-yearly_n <- collection %>%
-  group_by(Year) %>%
-  summarize(n = n()) %>%
-  filter(Year >= 1980)
-collections_plot <- ggplot(yearly_n, aes(x = Year, y = n)) +
-  geom_bar(position = "dodge", stat = "identity") +
-  theme_sleek() +
-  xlab(" ") + ylab("Number of collections")
-collections_plot
+combine_diet <- function(pred_species, prey_species) {
+  # Read in and combine diet data by species ----------------------------------
   
+  # All collection info
+  collection <- read.csv(paste0(path, "collection_information_v4.csv"))
+  
+  # Filter predator dataset for just hake and collections from 1980 onwards
+  predator <- read.csv(paste0(path, "predator_information_v4.csv")) %>%
+    filter(Predator_Com_Name == pred_species) %>%
+    filter(Collection_ID > 58) %>%  # Years before 1980
+    filter(!is.na(FL_cm))  # Remove observations without fork lengths
+  
+  # Filter collection info by hake predator collection IDs
+  collection_pred <- read.csv(paste0(path, "collection_information_v4.csv")) %>%
+    filter(Collection_ID %in% predator$Collection_ID)
+  
+  # Filter prey composition dataset by hake predator IDs
+  prey_comp <- read.csv(paste0(path, "prey_composition_v4.csv")) %>%
+    filter(Predator_ID %in% predator$Predator_ID)
+  
+  # Filter prey size dataset by hake-predated-prey IDs
+  prey_size <-  read.csv(paste0(path, "prey_size_v4.csv")) %>%
+    filter(Prey_Comp_ID %in% prey_comp$Prey_Comp_ID)
+  
+  # Combine collection info with predator info
+  all_pred <- merge(collection, predator, all.y = TRUE)[, c("Month", 
+                                                            "Year", 
+                                                            "Latitude", 
+                                                            "Longitude",
+                                                            "Predator_ID", 
+                                                            "FL_cm", 
+                                                            "Predator_Weight_kg")]
+  
+  
+  all_prey <- merge(prey_comp, prey_size, all.y = TRUE)[, c("Prey_Com_Name", 
+                                                            "Predator_ID", 
+                                                            "Prey_Weight_g",
+                                                            "Prey_Maturity",
+                                                            "Prey_Length1")]
+  
+  # Combine pred & prey together
+  all_stomachs <- merge(all_pred, all_prey, all.y = TRUE)
+  
+  predated <- all_stomachs %>%
+    filter(Prey_Com_Name == prey_species)
+  
+  # Plot general trends in the data ------------------------------------------
+  # Overall number of collections
+  yearly_n <- collection %>%
+    group_by(Year) %>%
+    summarize(n = n()) %>%
+    filter(Year >= 1980)
+  
+  collections_plot <- ggplot(yearly_n, aes(x = Year, y = n)) +
+    geom_bar(position = "dodge", stat = "identity") +
+    theme_sleek() +
+    xlab(" ") + ylab("Number of collections")
+  
+  # Top prey items by occurrence and weight
+  high_wt <- prey_comp %>% 
+    group_by(Prey_Com_Name) %>% 
+    summarize(highest = sum(Prey_Weight_g)) %>%
+    slice_max(n = 10, order_by = highest)
+  
+  high_n <- prey_comp %>% 
+    group_by(Prey_Com_Name) %>% 
+    summarize(highest = n()) %>%
+    slice_max(n = 10, order_by = highest)
+  
+  highest <- rbind(cbind(high_wt, variable = rep("weight (top 10)", 10)), 
+                   cbind(high_n, variable = rep("occurrence (top 10)", 10)))
+  
+  prey_sp_plot <- ggplot(highest, aes(x = Prey_Com_Name, y = highest,
+                                      fill = ifelse(Prey_Com_Name == prey_species, "highlighted", "normal"))) +
+    geom_bar(position = "dodge", stat = "identity", show.legend = FALSE) +
+    coord_flip() +
+    scale_fill_viridis(discrete = TRUE, begin = 0.1, end = 0.9, direction = -1) +
+    theme_sleek() +
+    xlab(" ") + ylab(" ") +
+    facet_wrap(~ variable, scales = "free")
+  
+  return(list(all_pred, all_prey, predated, collections_plot, prey_sp_plot))
+  
+}
 
-# Top prey items by occurrence and weight
-high_wt <- prey_of_hake_comp %>% 
-  group_by(Prey_Com_Name) %>% 
-  summarize(highest = sum(Prey_Weight_g)) %>%
-  slice_max(n = 10, order_by = highest)
+hake_hake <- combine_diet("Pacific Hake", "Pacific Hake")
 
-high_n <- prey_of_hake_comp %>% 
-  group_by(Prey_Com_Name) %>% 
-  summarize(highest = n()) %>%
-  slice_max(n = 10, order_by = highest)
+# Look at plots
+hake_hake[[4]]  # number of collections
+hake_hake[[5]]  # top prey species
 
-highest <- rbind(cbind(high_wt, variable = rep("weight (top 10)", 10)), 
-                 cbind(high_n, variable = rep("occurrence (top 10)", 10)))
-
-prey_sp <- ggplot(highest, aes(x = Prey_Com_Name, y = highest,
-                               fill = ifelse(Prey_Com_Name == "Pacific Hake", "highlighted", "normal"))) +
-  geom_bar(position = "dodge", stat = "identity", show.legend = FALSE) +
-  coord_flip() +
-  scale_fill_viridis(discrete = TRUE, begin = 0.1, end = 0.9, direction = -1) +
-  theme_sleek() +
-  xlab(" ") + ylab(" ") +
-  facet_wrap(~ variable, scales = "free")
-prey_sp
-
-ggsave(filename = "plots/diet/hake_prey_species.png", prey_sp, 
+ggsave(filename = "plots/diet/hake_prey_species.png", hake_hake[[5]], 
        width=200, height=80, units="mm", dpi=300)
 
-# Lengths
-pred_fl <- ggplot(predator_hake, aes(x = FL_cm)) +
-  geom_histogram() +
-  xlab("fork length (cm)") + ylab(" ") +
-  theme_sleek()
-pred_fl
 
-prey_length <- ggplot(hake_hake, aes(x = (Prey_Length1/10))) +
-  geom_histogram() +
-  xlab("length (cm)") + ylab(" ") +
-  theme_sleek()
-prey_length
+# Look at instances of cannibalism in hake ------------------------------------
+# Label predator dataframe by instances of cannibalism & change order for plots
+pred_type <- hake_hake[[1]]
+pred_type$type <- ifelse(pred_type$Predator_ID %in% hake_hake[[3]]$Predator_ID, "cannibalistic", "general predator")
+pred_type$type <- relevel(factor(pred_type$type), "general predator")
 
 # Predator hake stomachs including prey hake by year
 cannibalism_all <- pred_type %>%
@@ -257,5 +251,5 @@ ggsave(filename = "plots/diet/location_timing.png", location_timing,
   
   
 ### Write predator & prey datasets to .csvs -----------------------------------
-write.csv(all_pred, "data/diet/Full dataset/full_hake_pred.csv")
-write.csv(all_prey, "data/diet/Full dataset/full_prey.csv")
+write.csv(hake_hake[[1]], "data/diet/Full dataset/full_hake_pred.csv")
+write.csv(hake_hake[[2]], "data/diet/Full dataset/full_prey.csv")
