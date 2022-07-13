6d1bab9e8f7667ccfe925169a5348d8c5f078aed
Sophia Wassermann <hi@sowasser.com>
2022-07-13 16:21:12 -0700

    Moved time-varying diet into Dirichlet sensitivity & removed old data

--- ceattle_dirichlet_sensitivity.R
+++ ceattle_dirichlet_sensitivity.R
@@ -28,21 +28,8 @@ hake_intrasp <- Rceattle::read_data(file = "data/hake_intrasp_220713.xlsx")
 dirichlet_all <- read.csv("data/diet/Dirichlet/Dirichlet_all_years.csv")
 dirichlet_90s <- read.csv("data/diet/Dirichlet/Dirichlet_90s.csv")
 dirichlet_recent <- read.csv("data/diet/Dirichlet/Dirichlet_recent.csv")
+intrasp_yearly <- read.csv("data/diet/diet_for_CEATTLE_yearly.csv")
 
-# # Plot stomach contents curves
-# # Pull out data from base intrasp run
-# wts <- hake_intrasp$UobsWtAge$Stomach_proportion_by_weight
-# wts_short <- wts[wts != 0]
-# prop <- as.data.frame(cbind(1:15, wt001, wt005, wt01, wt05, wts_short))
-# colnames(prop)[c(1, 6)] <- c("age", "Grant's")
-# prop_all <- melt(prop, id.vars = "age")
-# 
-# stomach_props <- ggplot(prop_all, aes(x=age, y=value, fill=variable)) +
-#   geom_bar(stat = "identity", position = "dodge") +
-#   theme_sleek() +
-#   scale_fill_viridis(discrete = TRUE, begin = 0.1, end = 0.9) +
-#   ylab("stomach proportion")
-# stomach_props
 
 
 # Adapt weight proportions to replace those in the excel file & run CEATTLE
@@ -62,6 +49,7 @@ run_ceattle <- function(df) {
 run_all <- run_ceattle(dirichlet_all)
 run_90s <- run_ceattle(dirichlet_90s)
 run_recent <- run_ceattle(dirichlet_recent)
+run_tv <- run_ceattle(intrasp_yearly)
 
 # Plot biomass in comparison to no diet diet run ---------------------------
 years <- 1980:2022
@@ -78,18 +66,19 @@ ceattle_biomass <- function(run, name) {
   return(all_biom)
 }
 
-test_biom <- cbind(ceattle_biomass(run_all, "CEATTLE - all years"),
-                   ceattle_biomass(run_90s, "CEATTLE - 1991-1999"),
-                   ceattle_biomass(run_recent, "CEATTLE - 2005-2019"))
-test_biom <- test_biom[, c(1:3, 6, 9)]
+test_biom <- cbind(ceattle_biomass(run_all, "Dirichlet - all years"),
+                   ceattle_biomass(run_90s, "Dirichlet - 1991-1999"),
+                   ceattle_biomass(run_recent, "Dirichlet - 2005-2019"),
+                   ceattle_biomass(run_tv, "time-varying diet"))
+test_biom <- test_biom[, c(1:3, 6, 9, 12)]
 
 # Combine with run of CEATTLE using original data and plot
-nodiet_biom <- read.csv("data/ceattle_nodiet_biomass.csv")
-colnames(nodiet_biom)[3] <- "CEATTLE - no diet"
+nodiet_biom <- read.csv("data/ceattle_nodiet_biom.csv")
+colnames(nodiet_biom)[3] <- "no diet"
 
 plot_biom <- function(df) {
   wide <- cbind(df, nodiet_biom[, 3])
-  colnames(wide)[(ncol(wide))] <- c("CEATTLE - no diet")
+  colnames(wide)[(ncol(wide))] <- c("no diet")
   biom <- melt(wide, id.vars = c("year", "type"))
   
   plot <- ggplot(biom, aes(x=year, y=value)) +
@@ -106,21 +95,22 @@ plot_biom <- function(df) {
 test_biom_plot <- plot_biom(test_biom)
 test_biom_plot
 
-ggsave(filename="plots/CEATTLE/nodietecies predation/Testing/test_dirichlet_biomass.png", test_biom_plot, 
+ggsave(filename="plots/CEATTLE/intraspecies predation/Testing/test_dirichlet_biomass.png", test_biom_plot, 
        bg = "transparent", width=170, height=120, units="mm", dpi=300)
 
 
 # Plot recruitment ------------------------------------------------------------
 R_test_all <- cbind(c(run_all$quantities$R), c(run_90s$quantities$R), 
-                    c(run_recent$quantities$R))
+                    c(run_recent$quantities$R), c(run_tv$quantities$R))
 
 nodiet_R <- read.csv("data/ceattle_nodiet_R.csv")
 R_test_wide <- as.data.frame(cbind(years, R_test_all, nodiet_R))
 colnames(R_test_wide) <- c("year",
-                           "CEATTLE - all years",
-                           "CEATTLE - 1991-1999",
-                           "CEATTLE - 2005-2019",
-                           "CEATTLE - no diet")
+                           "all years",
+                           "1991-1999",
+                           "2005-2019",
+                           "time-varying diet",
+                           "no diet")
 R_test <- melt(R_test_wide, id.vars = "year")
 
 plot_R <- function(df) {
@@ -139,7 +129,7 @@ plot_R <- function(df) {
 test_R_plot <- plot_R(R_test)
 test_R_plot
 
-ggsave(filename="plots/CEATTLE/nodietecies predation/Testing/test_dirichlet_R.png", test_R_plot, 
+ggsave(filename="plots/CEATTLE/intraspecies predation/Testing/test_dirichlet_R.png", test_R_plot, 
        bg = "transparent", width=200, height=100, units="mm", dpi=300)
 
 
@@ -179,12 +169,13 @@ extract_nbyage <- function(run, name) {
 }
 
 nodiet_nbyage <- read.csv("data/ceattle_nodiet_nbyage.csv")[, -4]
-nodiet_nbyage <- cbind(nodiet_nbyage, rep("CEATTLE - no diet"))
+nodiet_nbyage <- cbind(nodiet_nbyage, rep("no diet"))
 colnames(nodiet_nbyage)[4] <- "model"
 
-nbyage_test_all <- rbind(extract_nbyage(run_all, "CEATTLE - all years"),
-                         extract_nbyage(run_90s, "CEATTLE - 1991-1999"),
-                         extract_nbyage(run_recent, "CEATTLE - 2005-2019"),
+nbyage_test_all <- rbind(extract_nbyage(run_all, "all years"),
+                         extract_nbyage(run_90s, "1991-1999"),
+                         extract_nbyage(run_recent, "2005-2019"),
+                         extract_nbyage(run_tv, "time-varying diet"),
                          nodiet_nbyage)
 
 # Set 15 as accumulation age
@@ -196,10 +187,11 @@ nbyage_test_mean <- nbyage_test_all %>% group_by(age, model) %>%
 
 # Change order of models to match other outputs
 nbyage_test_mean$model <- factor(nbyage_test_mean$model, 
-                                 levels = c("CEATTLE - all years", 
-                                            "CEATTLE - 1991-1999",
-                                            "CEATTLE - 2005-2019",
-                                            "CEATTLE - no diet"))
+                                 levels = c("all years", 
+                                            "1991-1999",
+                                            "2005-2019",
+                                            "time-varying diet",
+                                            "no diet"))
 
 test_nbyage_plot <- ggplot(nbyage_test_mean, aes(x=age, y=mean_number, fill=model)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -208,15 +200,18 @@ test_nbyage_plot <- ggplot(nbyage_test_mean, aes(x=age, y=mean_number, fill=mode
   xlab("age") + ylab("numbers") 
 test_nbyage_plot
 
-ggsave(filename = "plots/CEATTLE/nodietecies predation/Testing/test_dirichlet_nbyage.png", test_nbyage_plot, 
+ggsave(filename = "plots/CEATTLE/intraspecies predation/Testing/test_dirichlet_nbyage.png", test_nbyage_plot, 
        bg = "transparent", width=200, height=100, units="mm", dpi=300)
 
 
 ### Plot mortality ------------------------------------------------------------
-m_all <- plot_mortality(Rceattle = run_all, type = 0, zlim = c(0, 3.2), title = "all years", maxage = 15) # Mortality-at-age time series
-m_90s <- plot_mortality(Rceattle = run_90s, type = 0, zlim = c(0, 3.2), title = "1991-1999", maxage = 15) # Mortality-at-age time series
-m_recent <- plot_mortality(Rceattle = run_recent, type = 0, zlim = c(0, 3.2), title = "2005-2019", maxage = 15) # Mortality-at-age time series
+m_all <- plot_mortality(Rceattle = run_all, type = 0, zlim = c(0, 3.2), title = "all years", maxage = 15) 
+m_90s <- plot_mortality(Rceattle = run_90s, type = 0, zlim = c(0, 3.2), title = "1991-1999", maxage = 15) 
+m_recent <- plot_mortality(Rceattle = run_recent, type = 0, zlim = c(0, 3.2), title = "2005-2019", maxage = 15) 
+m_tv <- plot_mortality(Rceattle = run_tv, type = 0, zlim = c(0, 3.2), title = "time-varying diet", maxage = 15)
+
 
 ggsave(filename = "plots/CEATTLE/intraspecies predation/Testing/M_dirichlet_all.png", m_all, dpi=300)
 ggsave(filename = "plots/CEATTLE/intraspecies predation/Testing/M_dirichlet_90s.png", m_90s, dpi=300)
 ggsave(filename = "plots/CEATTLE/intraspecies predation/Testing/M_dirichlet_recent.png", m_recent, dpi=300)
+ggsave(filename = "plots/CEATTLE/intraspecies predation/Testing/M_dirichlet_recent.png", m_tv, dpi=300)
