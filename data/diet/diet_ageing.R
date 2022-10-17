00afee79ce78ac9fc415fcfbcb49de91d9b3d70a
Sophia Wassermann <hi@sowasser.com>
2022-10-17 13:32:17 -0700

    Using reciprocal VBGC to predict ages, now.

--- data/diet/diet_ageing.R
+++ data/diet/diet_ageing.R
@@ -17,23 +17,22 @@ all_prey <- read.csv("data/diet/CCTD/hake_prey.csv")
 
 
 ### Parameterize length to age calculation  -----------------------------------
-hake_ages <- 0:20
-
 # Read in maturity data
 maturity <- read.csv("~/Desktop/Local/hake-CEATTLE/Resources/hake-assessment-master/data/hake-maturity-data.csv")
-
 age_length <- data.frame(na.omit(cbind(Age = maturity$Age, Length = maturity$Length_cm)))
 
-like <- function(logLinf,logK,a0,logSigma)
-{
+hake_ages <- 0:20
+hake_lengths <- min(age_length$Length):max(age_length$Length)
+
+like <- function(logLinf,logK,a0,logSigma) {
   # Extract the parameters
-  Linf <- exp(logLinf); Kappa <- exp(logK); Sigma <- exp(logSigma)
+  Linf <- exp(logLinf); K <- exp(logK); Sigma <- exp(logSigma)
   
   # make the model predictions
-  Pred <- Linf*(1.0-exp(-Kappa*(Ages-a0)))
+  Pred <- (-log(1 - Lengths/Linf) / K) + a0
   
   # Compute the negative log-likelihood
-  NegLogL <- -1*sum(dnorm(Lengths,Pred,Sigma,TRUE))
+  NegLogL <- -1*sum(dnorm(Ages,Pred,Sigma,TRUE))
   
   return(NegLogL)
 }  
@@ -56,58 +55,41 @@ AIC(mleOutput)
 
 # Plot the model fit
 # Generate modelled data
-pred_lengths <- Linf*(1.0-exp(-K*(hake_ages-a0)))
-pred_data <- data.frame(hake_ages, pred_lengths)
+predicted_ages <- (-log(1 - hake_lengths/Linf) / K) + a0
+predicted_data <- data.frame(hake_lengths, predicted_ages)
 
-ggplot(pred_data, aes(x = hake_ages, y = pred_lengths)) +
+ggplot(predicted_data, aes(x = hake_lengths, y = predicted_ages)) +
   geom_line() +
-  geom_point(data = age_length, aes(x = Ages, y = Lengths))
+  geom_point(data = age_length, aes(x = Lengths, y = Age))
 
 
-# Calculate ages from lengths in dataset
-age_calc <- function(lengths, Linf, K, a0) {
-  ages <- c()
-  for(L in lengths) {
-    a <- max(1, ((-log(1 - L/Linf))/K + a0))
-    ages <- c(ages, a)
-  }
+### Calculate predator ages ---------------------------------------------------
+pred_ages <- (-log(1 - all_pred$FL_cm / Linf) / K) + a0
 
-  return(ages)
-}
-
-### Run predator age calculation ----------------------------------------------
-pred_ages <- age_calc(lengths = all_pred$FL_cm, 
-                      Linf = params[1], K = params[2], t0 = params[3])
-
-max(na.omit(pred_ages))
-min(na.omit(pred_ages))
+max(na.omit(pred_ages))  # check maximum
+min(na.omit(pred_ages))  # check minimum
+pred_ages[pred_ages < 1] <- 1  # replace values < 1 (lower accumulation age) <---- THIS SHOULD CHANGE WHEN AGE 0 WORKS
+pred_ages <- round(pred_ages, digits = 0)  # round to whole number
 
 # Add ages column to predator dataset 
 new_pred <- cbind(all_pred, pred_ages)
 
-# Check missing values
-nrow(new_pred %>% filter(FL_cm > params[1]))
-# Fill in age = 20 for any length > Linf
-new_pred$pred_ages[new_pred$FL_cm > params[1]] <- 20
-# Round to whole number 
-new_pred$pred_ages <- round(new_pred$pred_ages, digits = 0)
-# Set any ages > 20 to 20 (accumulator age)
-new_pred$pred_ages[new_pred$pred_ages > 20] <- 20
-
 
 ### Run prey age calculation --------------------------------------------------
-prey_ages <- age_calc(lengths = (all_prey$Prey_Length1 / 10),  # prey are in mm
-                      Linf = params[1], K = params[2], t0 = params[3]) 
+prey_ages <- (-log(1 - (all_prey$Prey_Length1 / 10) / Linf) / K) + a0
+
+max(na.omit(prey_ages))  # check maximum
+min(na.omit(prey_ages))  # check minimum
+prey_ages[prey_ages < 1] <- 1  # replace values < 1 (lower accumulation age) <---- THIS SHOULD CHANGE WHEN AGE 0 WORKS
+prey_ages <- round(prey_ages, digits = 0)  # round to whole number
 
 # Add ages column to prey dataset - includes non-hake prey
 new_prey <- cbind(all_prey, prey_ages)
-# Round to whole number 
-new_prey$prey_ages <- round(new_prey$prey_ages, digits = 0)
 # Filter prey dataset to only include hake
 new_hake_prey <- new_prey %>% filter(Prey_Com_Name == "Pacific Hake")
 
 
-# Read in FEAT data -----------------------------------------------------------
+### Read in FEAT data and compare fit -----------------------------------------
 FEAT_data <- read.csv("data/diet/FEAT/all_FEAT_diet.csv")
 FEAT_prey_lengths <- read.csv("data/diet/FEAT/hake_prey_lengths_FEAT.csv")
 
@@ -116,8 +98,8 @@ FEAT_hake <- merge(FEAT_data, FEAT_prey_lengths, by = "stomach_uuid", all.x = TR
   filter(prey_category_long == "Gadiformes") %>%
   select(predator_length_cm, predator_age, measure_value)
 
-FEAT_hake$prey_ages <- age_calc(lengths = FEAT_hake$measure_value,
-                                Linf = params[1], K = params[2], t0 = params[3])
+FEAT_hake$prey_ages <- (-log(1 - FEAT_hake$measure_value/Linf) / K) + a0
+FEAT_hake$prey_ages[FEAT_hake$prey_ages < 1] <- 1  # replace values < 1 (lower accumulation age) <---- THIS SHOULD CHANGE WHEN AGE 0 WORKS
 
 FEAT_ages <- as.data.frame(rbind(cbind(age = FEAT_hake$predator_age, length = FEAT_hake$predator_length_cm,
                                        data = rep("FEAT predators", length(FEAT_hake$predator_ages)))))
@@ -144,7 +126,7 @@ all_ages$length <- as.numeric(all_ages$length)
 all_ages <- na.omit(all_ages)
 
 growth_curve <- ggplot(all_ages, aes(x = age, y = length, color = data, shape = data)) +
-  geom_point(alpha = 0.3, size = 3) +
+  geom_point(alpha = 0.4, size = 3) +
   scale_color_viridis(discrete = TRUE, direction = -1, begin = 0.1, end = 0.9)
 growth_curve
 
