82bf326f3d76f2ad368075f564cbfbf03af4a2c8
Sophia Wassermann <hi@sowasser.com>
2023-05-03 12:49:01 -0700

    Updated CEATTLE running script for using the dev branch

--- run_ceattle.R
+++ run_ceattle.R
@@ -14,7 +14,7 @@ hake_intrasp <- Rceattle::read_data(file = "data/hake_intrasp_230427.xlsx")
 
 ### Run and fit the CEATTLE model ---------------------------------------------
 run_CEATTLE <- function(data, M1, prior, init, msm, M_phase) {
-  data$est_M1 <- M1  # Set M1 to fixed (0), estimated (1), age-varying estimate (3)
+  data$est_M1 <- M1  
   # data$endyr <- 2019
   run <- fit_mod(data_list = data,
                  inits = init,
@@ -33,50 +33,53 @@ run_CEATTLE <- function(data, M1, prior, init, msm, M_phase) {
                                            Plimit = 0.1, # No fishing when SB<SB10
                                            Pstar = 0.45,
                                            Sigma = 0.5),
-                 # phase = "default",
-                phase = list(
-                  dummy = 1,
-                  ln_pop_scalar = 4,
-                  ln_mean_rec = 1,
-                  ln_rec_sigma = 2,
-                  rec_dev = 2,
-                  init_dev = 2,
-                  ln_sex_ratio_sigma = 3,
-                  ln_M1 = M_phase,
-                  ln_mean_F = 1,
-                  ln_Flimit = 3,
-                  ln_Ftarget = 3,
-                  proj_F_prop = 1,
-                  F_dev = 1,
-                  ln_srv_q = 3,
-                  # srv_q_pow = 4,
-                  ln_srv_q_dev = 5,
-                  ln_sigma_srv_q = 4,
-                  ln_sigma_time_varying_srv_q = 4,
-                  sel_coff = 3,
-                  sel_coff_dev = 4,
-                  ln_sel_slp = 3,
-                  sel_inf = 3,
-                  ln_sel_slp_dev = 5,
-                  sel_inf_dev = 5,
-                  ln_sigma_sel = 4,
-                  sel_curve_pen = 4,
-                  ln_sigma_srv_index = 2,
-                  ln_sigma_fsh_catch = 2,
-                  comp_weights = 4,
-                  logH_1 = 6,
-                  logH_1a = 6,
-                  logH_1b = 6,
-                  logH_2 = 6,
-                  logH_3 = 6,
-                  H_4 = 6,
-                  log_gam_a = 5,
-                  log_gam_b = 5,
-                  log_phi = 5
-                ),
-                # initMode = 1,
+                 M1Fun = Rceattle::build_M1(M1_model = M1, # Set M1 to fixed (0) or estimated (1)
+                                            updateM1 = TRUE),
+                 phase = "default",
+                # # Update phase to help convergence --------------------------
+                # phase = list(
+                #   dummy = 1,
+                #   ln_pop_scalar = 4,
+                #   ln_mean_rec = 1,
+                #   ln_rec_sigma = 2,
+                #   rec_dev = 2,
+                #   init_dev = 2,
+                #   ln_sex_ratio_sigma = 3,
+                #   ln_M1 = M_phase,
+                #   ln_mean_F = 1,
+                #   ln_Flimit = 3,
+                #   ln_Ftarget = 3,
+                #   proj_F_prop = 1,
+                #   F_dev = 1,
+                #   ln_srv_q = 3,
+                #   # srv_q_pow = 4,
+                #   ln_srv_q_dev = 5,
+                #   ln_sigma_srv_q = 4,
+                #   ln_sigma_time_varying_srv_q = 4,
+                #   sel_coff = 3,
+                #   sel_coff_dev = 4,
+                #   ln_sel_slp = 3,
+                #   sel_inf = 3,
+                #   ln_sel_slp_dev = 5,
+                #   sel_inf_dev = 5,
+                #   ln_sigma_sel = 4,
+                #   sel_curve_pen = 4,
+                #   ln_sigma_srv_index = 2,
+                #   ln_sigma_fsh_catch = 2,
+                #   comp_weights = 4,
+                #   logH_1 = 6,
+                #   logH_1a = 6,
+                #   logH_1b = 6,
+                #   logH_2 = 6,
+                #   logH_3 = 6,
+                #   H_4 = 6,
+                #   log_gam_a = 5,
+                #   log_gam_b = 5,
+                #   log_phi = 5),
+                # # -----------------------------------------------------------
                 # verbose = 1,
-                projection_uncertainty = TRUE)
+                initMode = 1,
+                projection_uncertainty = TRUE) 
   
   objective <- run$opt$objective
   jnll <- run$quantities$jnll
@@ -100,6 +103,7 @@ ss_fixM1 <- run_CEATTLE(data = hake_intrasp,
                         msm = 0, 
                         M_phase = 6)
 ss_fixM1$fit  # check convergence
+save(ss_fixM1, file = "models/ss_fixM1.Rdata")
 
 ss_estM1 <- run_CEATTLE(data = hake_intrasp, 
                         M1 = 1, 
@@ -109,6 +113,7 @@ ss_estM1 <- run_CEATTLE(data = hake_intrasp,
                         M_phase = 1)
 ss_estM1$fit  # check convergence
 ss_estM1$model$quantities$M1
+save(ss_estM1, file = "models/ss_estM1.Rdata")
 
 # ss_priorM1 <- run_CEATTLE(data = hake_intrasp, 
 #                           M1 = 1, 
@@ -127,28 +132,22 @@ ms_estM1 <- run_CEATTLE(data = hake_intrasp,
                         msm = 1, 
                         M_phase = 1)
 ms_estM1$fit  # check convergence
-
 # Rceattle diagnostic plots 
 Rceattle::plot_biomass(ms_estM1$model, add_ci = TRUE)
 Rceattle::plot_recruitment(ms_estM1$model, add_ci = TRUE, incl_proj = TRUE)
+save(ms_estM1, file = "models/ms_estM1.Rdata")
 
 ms_fixM1 <- run_CEATTLE(data = hake_intrasp, 
-                        M1 = 1, 
+                        M1 = 0, 
                         prior = FALSE, 
                         init = ss_fixM1$model$estimated_params, 
                         msm = 1, 
                         M_phase = 6)
 ms_fixM1$fit  # check convergence
-
-
-### Save converged models -----------------------------------------------------
-save(ss_fixM1, file = "models/ss_fixM1.Rdata")
-save(ss_estM1, file = "models/ss_estM1.Rdata")
-save(ms_estM1, file = "models/ms_estM1.Rdata")
 save(ms_fixM1, file = "models/ms_fixM1.Rdata")
 
 
-# For future reference: age-blocked mortality at 1, 3, and 3+
+# For future reference: age-blocked mortality at 1, 3, and 3+ -----------------
 # map <- intrasp_run$map
 # orig <- intrasp_run$map$mapList$ln_M1
 # orig[orig > 2] <- 3
