8faf5c4c5a47d655b7cd882c93aff8cb22ef59cb
Sophia Wassermann <hi@sowasser.com>
2023-05-05 19:18:28 -0700

    Added multispecies model to profile code

--- profile_Rceattle.R
+++ profile_Rceattle.R
@@ -2,10 +2,10 @@
 
 library(Rceattle)
 library(ggplot2)
+library(dplyr)
 
 data <- read_data(file = "data/hake_intrasp_230427.xlsx")  # Read in data
 
-### Run profile over M1 in single-species model -------------------------------
 M_vec <- seq(0.15, 0.35, 0.01)  # Initial vector of M values to test
 
 # Function updating M1 for each run of the model
@@ -33,43 +33,91 @@ get_profile <- function(M1_change, msm) {
   )
   
   # Save the resulting run to losing progress to R bombs!
-  save(run, file = paste0("models/profile/run", as.character(M1_change), ".Rdata"))
+  if (msm == 0) {save(run, file = paste0("models/profile/ss/run", as.character(M1_change), ".Rdata"))}
+  if (msm == 1) {save(run, file = paste0("models/profile/ms/run", as.character(M1_change), ".Rdata"))}
 }
 
+### Run profile over M1 in single-species model -------------------------------
 # Run the function for all values in M_vec
 for(i in 1:length(M_vec)) {
   get_profile(M_vec[i], msm = 0)
 }
-
 
 # Update M_vec as needed if function/R crashes, or for more values
 M_vec_new <- c(0.33)
 
 # Run the function for all values in M_vec
 for(i in 1:length(M_vec_new)) {
-  get_profile(M_vec_new[i])
+  get_profile(M_vec_new[i], msm = 0)
 }
 
-# Compare likelihoods & plot --------------------------------------------------
-runs <- list.files(path = "models/profile")  # List of all model runs
+# Get JNLL for each run 
+runs <- list.files(path = "models/profile/ss")  # List of all model runs
 
 # Get sum of joint negative log likelihood for each run
-jnll_sums <- c() 
+jnll_sums_ss <- c() 
 for(i in 1:length(runs)) {
-  load(paste0("models/profile/", runs[i]))
+  load(paste0("models/profile/ss/", runs[i]))
   jnll_summary <- as.data.frame(run$quantities$jnll_comp)
   jnll_summary$sum <- rowSums(run$quantities$jnll_comp)
-  jnll_sums[i] <- sum(rowSums(run$quantities$jnll_comp))
+  jnll_sums_ss[i] <- sum(rowSums(run$quantities$jnll_comp))
 }
 
 # Combine with M1 input for each run
-profile <- cbind.data.frame(M1 = M_vec,
-                            JNLL = jnll_sums)
+profile_ss <- cbind.data.frame(M1 = M_vec,
+                               JNLL = jnll_sums_ss,
+                               model = "single-species")
 
-# Convert to range from 0-1
-range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
-ggplot(profile, aes(x = M1, y = range01(JNLL))) +
+### Run profile over M1 in cannibalism model ----------------------------------
+# Run the function for all values in M_vec
+for(i in 1:length(M_vec)) {
+  get_profile(M_vec[i], msm = 1)
+}
+
+# Update M_vec as needed if function/R crashes, or for more values
+M_vec_new <- c(0.24)
+
+# Run the function for all values in M_vec
+for(i in 1:length(M_vec_new)) {
+  get_profile(M_vec_new[i], msm = 1)
+}
+
+# Get JNLL for each run & plot with M1 value
+runs <- list.files(path = "models/profile/ms")  # List of all model runs
+
+# Get sum of joint negative log likelihood for each run
+jnll_sums_ms <- c() 
+for(i in 1:length(runs)) {
+  load(paste0("models/profile/ms/", runs[i]))
+  jnll_summary <- as.data.frame(run$quantities$jnll_comp)
+  jnll_summary$sum <- rowSums(run$quantities$jnll_comp)
+  jnll_sums_ms[i] <- sum(rowSums(run$quantities$jnll_comp))
+}
+
+# Combine with M1 input for each run
+profile_ms <- cbind.data.frame(M1 = M_vec,
+                               JNLL = jnll_sums_ms,
+                               model = "cannibalism")
+
+
+### Plot profile --------------------------------------------------------------
+est_M1 <- cbind.data.frame(value = c(0.257, 0.318),
+                           model = c("single-species", "cannibalism"))
+est_M1$model <- factor(est_M1$model, levels = c("single-species", "cannibalism"))
+
+profile_plot <- rbind(profile_ss, profile_ms) %>%
+  mutate(model = factor(model, levels = c("single-species", "cannibalism"))) %>%
+  ggplot(., aes(x = M1, y = JNLL)) +
+  geom_point() +
   geom_line() +
-  ylab("JNLL") +
-  ggsidekick::theme_sleek()
+  geom_vline(xintercept = 0.21, linetype = "dotted") +
+  geom_vline(data = est_M1, mapping = aes(xintercept = value, color = model), linetype = "dashed") +
+  ylab("JNLL") + labs(color = "M1 estimate") +
+  ggsidekick::theme_sleek() +
+  facet_wrap(~model)
+profile_plot
+
+ggsave(filename="plots/CEATTLE/cannibalism/M1_profile.png", 
+       profile_plot, 
+       width=180, height=80, units="mm", dpi=300)
