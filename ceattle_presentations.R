8d189d7f7a81d9670b016a78ab1ca3b7afa9cc01
Sophia Wassermann <hi@sowasser.com>
2023-08-29 21:29:09 -1000

    More plots included in presentation plot script

--- ceattle_presentations.R
+++ ceattle_presentations.R
@@ -443,6 +443,93 @@ diet_plot <- ggplot(hake_prop, aes(x=as.factor(predator_age), y=wt_prop, fill=as
   labs(fill = "prey hake age")
 diet_plot
 
+### Instances of cannibalism --------------------------------------------------
+# Read in & update CCTD data from SWFSC 
+CCTD_pred <- read.csv("data/diet/CCTD/hake_aged_pred.csv")
+CCTD_prey <- read.csv("data/diet/CCTD/hake_aged_prey.csv")
+
+# Combine predators and prey together, generalize prey names to "other"
+CCTD_all <- merge(CCTD_pred, CCTD_prey, by = "Predator_ID") %>%
+  mutate(prey_name = ifelse(Prey_Com_Name == "Pacific Hake", "Pacific Hake", "other")) %>%
+  select(Predator_ID, Month, Year, Latitude, Longitude, pred_ages, prey_name, 
+         Prey_Weight_g, Prey_Length1, prey_ages) %>%
+  filter(Year <= 2004)  # remove years covered by FEAT 
+
+# Add column for source of data
+CCTD_all <- cbind(CCTD_all, source = rep("CCTD", length(CCTD_all[, 1])))
+
+
+### Read in & update FEAT data from NWFSC -------------------------------------
+FEAT_data <- read.csv("data/diet/FEAT/all_FEAT_diet.csv")
+FEAT_prey_lengths <- read.csv("data/diet/FEAT/hake_prey_lengths_FEAT.csv")
+
+# Add prey lengths based on stomach ID
+FEAT_all <- merge(FEAT_data, FEAT_prey_lengths, by = "stomach_uuid", all.x = TRUE)
+
+# Expand to month and year columns
+FEAT_all$tow_timestamp <- as.Date(FEAT_all$tow_timestamp)
+FEAT_all$month <- lubridate::month(lubridate::ymd(FEAT_all$tow_timestamp))
+FEAT_all$year <- lubridate::year(lubridate::ymd(FEAT_all$tow_timestamp))
+
+# Generalize prey categories to Pacific Hake & "other", select columns
+FEAT_all <- FEAT_all %>%
+  mutate(prey_name = ifelse(prey_category_long == "Gadiformes", "Pacific Hake", "other")) %>%
+  select(stomach_uuid, month, year, tow_latitude, tow_longitude, predator_age, 
+         prey_name, content_wt_g, measure_value)
+
+# Switch to age 20 accumulator age
+FEAT_all <- FEAT_all %>% 
+  filter(predator_age != "(blank)") %>%
+  mutate(predator_age = as.numeric(predator_age)) %>%
+  mutate(predator_age = ifelse(predator_age > 20, 20, predator_age))
+
+#Combine datasets
+# Add column for prey ages. All are age 1 according to calculations in the 
+# diet_aging.R script
+FEAT_all <- cbind(FEAT_all, 
+                  prey_ages = rep(1, length(FEAT_all[, 1])),
+                  source = rep("FEAT", length(FEAT_all[, 1])))
+
+labels <- c("Predator_ID", "month", "year", "latitude", "longitude", "predator_age", 
+            "prey_name", "prey_wt", "prey_length", "prey_age", "source")
+
+colnames(CCTD_all) <- labels
+colnames(FEAT_all) <- labels
+
+all_data <- rbind(CCTD_all, FEAT_all)
+
+# Remove all prey ages for prey that isn't hake & remove 1980 (year w/ no ages)
+all_data$prey_age[all_data$prey_name == "other"] <- NA
+
+hake_data <- all_data %>%
+  filter(prey_name == "Pacific Hake") 
+
+# Look at instances of no age - all weights within year 1 range except for 1
+no_age <- hake_data %>% filter(is.na(prey_length))
+# Replace NAs with age 1
+all_data$prey_age[is.na(all_data$prey_age) & all_data$prey_name == "Pacific Hake"] <- 1 
+
+# Add estimate of correct age for very big hake prey
+all_data$prey_age[all_data$prey_wt > 300 & all_data$prey_name == "Pacific Hake"] <- 5
+
+# Rewmove 1980 w/ no ages
+all_data <- all_data %>% filter(year != 1980)
+
+# Write combined dataset
+# write.csv(all_data, "data/diet/CCTD_FEAT_combined.csv", row.names = FALSE)
+
+# Plot timing & location of instances of predation of interest
+predation_all <- all_data %>%
+  group_by(year, prey_name) %>%
+  summarize(n = n()) %>%
+  filter(!is.na(year))
+
+predation_yearly <- ggplot(predation_all, aes(x = year, y = n, fill = prey_name)) +
+  geom_bar(position = "stack", stat = "identity") +
+  scale_fill_viridis(discrete = TRUE, option = "plasma", begin = 0.2) +
+  labs(fill = "prey species") + ylab("stomachs (n)") + xlab(" ")
+predation_yearly
+
 ### Save plots (when not experimenting) ---------------------------------------
 ggsave(filename="plots/presentations/popdyn_M1prior.png", plots$popdy, 
        width=200, height=90, units="mm", dpi=300, bg = "transparent")
@@ -460,3 +547,5 @@ ggsave(filename="plots/presentations/mean_temp_compared.png", mean_temp_compared
        bg = "transparent", width=180, height=90, units="mm", dpi=300)
 ggsave(filename = "plots/presentations/cannibalism_overall.png", diet_plot, 
        bg = "transparent", width=200, height=120, units="mm", dpi=300)
+ggsave(filename = "plots/presentations/hake_cannibalism.png", predation_yearly,
+       bg = "transparent", width=190, height=100, units="mm", dpi=300)
